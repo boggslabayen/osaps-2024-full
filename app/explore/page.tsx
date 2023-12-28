657e3d2b74b70d8b5093eafe56c8656ae36d8e5b
Jose ildefonso Labayen <boggslabayen@Joses-MacBook-Pro.local>
2023-12-28 23:56:29 +0800

    fixed props

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -11,11 +11,11 @@ export default function page(){
     return(
         <div>
         <StickyNavbar />
-        <BannerWithImage
+        {/* <BannerWithImage
         header="Unwind and Explore Mactan's Treasures Beyond OSAPS 2024!"
         subheader="Mactan, Cebu is a captivating island paradise nestled in the Philippines, renowned for its pristine beaches, crystal-clear waters, and vibrant marine life. This tropical haven offers an array of water sports, historic sites like the Magellan Shrine, and the striking Mactan-Cebu International Airport. It's a destination where natural beauty and rich culture converge, providing travelers with an unforgettable experience in the heart of the Visayas region."
         imageSource={Picture}
-        />
+        /> */}
         </div>
     )
 }
