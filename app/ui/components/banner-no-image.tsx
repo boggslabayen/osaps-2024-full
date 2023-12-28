b2d54704dc75e0fc2f6a04f7cc165b679ddd7eba
Jose ildefonso Labayen <boggslabayen@Joses-MacBook-Pro.local>
2023-12-28 23:59:55 +0800

    3

--- app/ui/components/banner-no-image.tsx
+++ app/ui/components/banner-no-image.tsx
@@ -8,7 +8,7 @@ interface BannerNoImageProps {
   }
 
 
-const BannerNoImage: React.FC<BannerNoImageProps> = (props) => {
+const BannerNoImage: React.FC<BannerNoImageProps> = (props: any) => {
     return (
         <div className='bg-ivory py-20 flex justify-center'>
         <div className=' max-w-screen mx-8 lg:max-w-screen-xl'>
