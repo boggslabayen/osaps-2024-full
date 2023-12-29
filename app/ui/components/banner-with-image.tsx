5a4cd693d589a899ae1fde50d6269945f106f03e
Jose ildefonso Labayen <boggslabayen@Joses-MacBook-Pro.local>
2023-12-29 21:33:00 +0800

    fixed banners

--- app/ui/components/banner-with-image.tsx
+++ app/ui/components/banner-with-image.tsx
@@ -2,35 +2,45 @@
 import * as React from 'react'
 import Image from 'next/image'
 
+interface BannerWithImageProps {
+    header: string;
+    subheader: string;
+    imageSource: string;
+    imageAlt: string;
+  }
 
-export default function BannerWithImage(props: any) {
+  const BannerWithImage: React.FC<BannerWithImageProps> = (props) => {
     return (
+       
         <div className='bg-ivory py-20 flex justify-center'>
 
             <div className='max-w-screen mx-8 lg:max-w-screen-xl flex justify-between items-center'>
 
-            <div className='w-full lg:w-2/3'>
-            <h1 className='text-blue-jay font-extrabold text-4xl'>
-                {props.header}
-            </h1>
+             <div className='w-full lg:w-2/3'>
+             <h1 className='text-blue-jay font-extrabold text-4xl'>
+                 {props.header}
+             </h1>
             <p className='text-gray-800 my-8 text-xl leading-10 font-normal'>{props.subheader}</p>
-            </div>
+             </div>
 
-            <Image 
-            src={props.imageSource}
-            width={607}
-            height={562}
-            className='w-1/2 hidden lg:block'
-            alt={props.imageAlt}
-            />
+             <Image 
+             src={props.imageSource}
+             width={607}
+             height={562}
+             className='w-1/2 hidden lg:block'
+             alt={props.imageAlt}
+             />
 
-            </div>
+             </div>
+
+        </div>
+
+    )
+  };
+
+    export default BannerWithImage;
             
         
             
             
-            
-    
-        </div>
-    )
-}
+      
