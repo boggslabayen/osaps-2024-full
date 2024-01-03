7406ab93e5290dc3ac1287c6beb794ee29f7c5dd
Jose ildefonso Labayen <boggslabayen@Joses-MacBook-Pro.local>
2024-01-03 12:56:56 +0800

    Fixing the images

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -66,7 +66,7 @@ export default function page(){
         <BannerWithImage 
         header='Harmony in Diversity'
         subheader="The Oriental Society of Aesthetic Plastic Surgery (OSAPS) is dedicated to advancing the concept of Oriental beauty, offering ongoing high-quality education to plastic surgeons regarding the latest procedures and ethical patient care. This year's 18th international congress, held in collaboration with the Philippine Association of Plastic and Reconstructive and Aesthetic Surgeons (PAPRAS) during their 11th National Meeting, serves as a valuable platform for the global exchange of knowledge in aesthetic surgery. The discussions revolve around how Oriental beauty is shaping the international standard in aesthetic plastic surgery, encapsulated by the theme Harmony in Diversity."
-        src={Picture.src}
+        src={Picture}
         imageAlt='Cebu Picture'
         />
 
